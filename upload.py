ee09ad7da1f150187110f9df58e06a672ae254c6
fgh09101010 <fgh09101010@gmail.com>
2025-08-04 21:11:22 +0800

    更新 .gitignore 檔案以排除 docs/ 目錄；改善 upload.py 檔案中的註解以說明 client_secrets.json 的使用

--- upload.py
+++ upload.py
@@ -4,7 +4,9 @@ import pandas as pd
 from datetime import datetime
 import os
 
-# 1. 認證
+# 1. 認證 需要有 client_secrets.json 檔案
+#    這個檔案可以從 Google Cloud Console 下載，並放在與此腳本同一目錄下。
+#    如果沒有這個檔案，請參考 PyDrive 的官方文檔來建立。
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
 drive = GoogleDrive(gauth)
